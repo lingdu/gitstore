340486430c392982eae10721fbc6957a111f7839
836036098 <836036098@qq.com>
2015-04-24 20:34:50 +0800

    2015/4/24 lj test

--- src/com/memristor/baidumap/MainActivity.java
+++ src/com/memristor/baidumap/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//¿×ÖÛ·«µ÷ÊÔ
+//ÂŞ½Üµ÷ÊÔ
 @SuppressWarnings("unused")
 public class MainActivity extends Activity {  
     MapView mMapView = null;  
