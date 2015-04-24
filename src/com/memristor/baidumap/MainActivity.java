d261cefd9d037e7419d3511e3e999951a4e9809d
gxz <swugxz@163.com>
2015-04-24 20:10:57 +0800

    2015/4/24 gxz test push

--- src/com/memristor/baidumap/MainActivity.java
+++ src/com/memristor/baidumap/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//测试alexjzy协作push
+//测试guoxiuzhen协作push
 @SuppressWarnings("unused")
 public class MainActivity extends Activity {  
     MapView mMapView = null;  
